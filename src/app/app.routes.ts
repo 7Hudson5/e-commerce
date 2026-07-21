eb280f44fd2ae894463f77bf794e8f99062a8678
adam castro <riuksilva72@gmail.com>
2026-07-21 16:30:47 -0300

    refactor(routes): adiciona nova importação, substitui components e refaz o path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,64 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
-import { Component } from '@angular/core';
+// // import { Routes } from '@angular/router';
+// // //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos'; //codigo legado
+// // import { Carrinho } from './features/carrinho/carrinho/carrinho';
+// // //import { Component } from '@angular/core';
+// // import { Home } from './features/home/home/home';
+// // //import path from 'path';
+// // //add um novo import
+// // export const routes: Routes = [
 
+// //     //
+// //     {
+// //         path:'',
+// //         component: Home, 
+// //     },
+// // //cODIGO LEGADO LAZYLOADING
+// //     //{
+// //     //    path:'produtos',
+// //     //    component: ListaProdutos,
+// //     //},
+
+// //     {
+// //           path:'produtos',
+// //           loadcomponent: () => 
+// //             import ('./features/produtos/lista-produtos/lista-produtos'.then(m =>m.ListaProdutos))
+
+// //     },
+// // {
+// //        path:'carrinho',
+// //        component: Carrinho,
+// // },
+// // ]
+// código final lazyloading e loadcomponents
+
+import { Routes } from "@angular/router";
 export const routes: Routes = [
-    {
-        path:'',
-        component: ListaProdutos, 
-    },
-    {
-        path:'carrinho',
-        component: Carrinho,
-    },
+
+{
+   path:'',
+   loadComponent: () =>
+       import('./features/home/home/home').then((m) =>m.Home),
+
+},
+
+{
+   path:'produtos',
+   loadComponent: () =>
+       import('./features/produtos/lista-produtos/lista-produtos').then((m) =>m.ListaProdutos),
+
+},
+
+{
+   path:'carrinho',
+   loadComponent: () =>
+       import('./features/carrinho/carrinho/carrinho').then((m) =>m.Carrinho),
+
+},
+
+{
+   path:'**',
+   redirectTo: '',
+
+},
+
 ];
